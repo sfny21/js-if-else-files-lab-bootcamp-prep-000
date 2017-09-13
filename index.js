505b4a3eff439ba7fad98eb67d4964d0cdbdeee5
Gilbert Lamphere gilbert.lamphere@gmail.com <gilbert.lamphere@gmail.com>
2017-09-13 17:55:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,6 +5,11 @@ if (name==='Susan'){
 } else {
   console.log('Expected the name to be Susan')
 }
+if (height==='74'){
+  console.log('The height is correct')
+} else {
+  console.log('The height is incorrect')
+}
 
 // Don't worry about this
 module.exports = { name, height
